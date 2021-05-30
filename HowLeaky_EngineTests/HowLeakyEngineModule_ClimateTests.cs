d2185e6a8050c7e8a86bed199c375f69d3e03b1b
david-at-dhm <58316340+david-at-dhm@users.noreply.github.com>
2021-05-30 10:38:43 +1000

    Updated Namespace and fixed some assertions

--- HowLeaky_EngineTests/HowLeakyEngineModule_ClimateTests.cs
+++ HowLeaky_EngineTests/HowLeakyEngineModule_ClimateTests.cs
@@ -6,7 +6,7 @@ using NUnit.Framework;
 using System;
 using System.Collections.Generic;
 
-namespace UnitTestProject1
+namespace HowLeakyEngineTests
 {
     [TestFixture]
     public class HowLeakyEngineModule_ClimateTests
@@ -16,7 +16,7 @@ namespace UnitTestProject1
         [Test]
         public void Simulate_DateInRange_AssignsAllVariables()
         {
-            //Arrange
+    
             var engine = new HowLeakyEngine();
             var climatemodule = new HowLeakyEngineModule_Climate();
             var inputmodule = new HowLeakyInputs_Climate();
@@ -28,29 +28,26 @@ namespace UnitTestProject1
             inputmodule.VP = new List<double>(new double[1000]);
             climatemodule.Engine = engine;
             climatemodule.InputModel = inputmodule;
-            //Act
+   
             inputmodule.StartDate = new BrowserDate(2019, 1, 1);
             engine.TodaysDate = new BrowserDate(2019, 3, 4);
 
-            //Assert
-            climatemodule.Simulate();
+       
+            Assert.DoesNotThrow(() => climatemodule.Simulate());
         }
 
         [Test]
         
         public void Simulate_DateOutOfRangeRange_ThrowsException()
         {
-            //Arrange
             var engine=new HowLeakyEngine();
             var climatemodule=new HowLeakyEngineModule_Climate();
             var inputmodule=new HowLeakyInputs_Climate();
             climatemodule.Engine=engine;
             climatemodule.InputModel=inputmodule;
-            //Act
             inputmodule.StartDate=new BrowserDate(2019,1,1);
             engine.TodaysDate=new BrowserDate(2018,3,4);     
-           
-           //Assert
+
             Assert.Throws<Exception>(() => climatemodule.Simulate());
    
         }
